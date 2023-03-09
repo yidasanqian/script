d4ae99def140fbcb343e5836f843fdebf000ad02
yidasanqian <yidasanqian@gmail.com>
2023-03-09 17:53:15 +0800

    日志

--- private/newbing.js
+++ private/newbing.js
@@ -2,6 +2,8 @@
 function Change_Request()
 {
     var RequestHeaders = $request.headers;
+    console.log(RequestHeaders);
     RequestHeaders["x-forwarded-for"] = "8.8.8.8";
+    console.log(RequestHeaders);
     $done({RequestHeaders});
 }
