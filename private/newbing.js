9093c564846d3abf75a83c6b3a10a4e065a3d4e4
yidasanqian <yidasanqian@gmail.com>
2023-03-10 17:33:00 +0800

    fake ua

--- private/newbing.js
+++ private/newbing.js
@@ -5,6 +5,7 @@ function Change_Request()
 {
     var RequestHeaders = $request.headers;
     console.log(RequestHeaders);
+    RequestHeaders["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41";
     RequestHeaders["x-forwarded-for"] = "8.8.8.8";
     console.log(RequestHeaders);
     $done({RequestHeaders});
