86f4046956d78b3c87bc9c38ec239999a536ddfe
yidasanqian <yidasanqian@gmail.com>
2023-03-10 17:36:45 +0800

    update requestheader

--- private/newbing.js
+++ private/newbing.js
@@ -5,8 +5,9 @@ function Change_Request()
 {
     var RequestHeaders = $request.headers;
     console.log(RequestHeaders);
-    RequestHeaders["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41";
-    RequestHeaders["x-forwarded-for"] = "8.8.8.8";
+    RequestHeaders["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41";
+    RequestHeaders["X-Forwarded-For"] = "8.8.8.8";
+    RequestHeaders["Host"] = "www.bing.com";
     console.log(RequestHeaders);
     $done({RequestHeaders});
 }
